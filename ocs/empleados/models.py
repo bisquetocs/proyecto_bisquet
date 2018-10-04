635440f1f913375eb506dd4cf40bfc8ffc8f1365
Fatima <A01209842@itesm.mx>
2018-10-03 21:35:39 -0500

    tablas

--- ocs/empleados/models.py
+++ ocs/empleados/models.py
@@ -1,3 +1,34 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Empresa(models.Model):
+    nombre_empresa = models.CharField(max_length=100)
+    razon_social = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nombre_empresa
+
+class Privilegio(models.Model):
+    nombre_privilegio = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nombre_privilegio
+
+class Rol(models.Model):
+    nombre_rol = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nombre_rol
+
+class RolPrivilegio(models.Model):
+    id_Rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
+    id_Privilegio = models.ForeignKey(Privilegio, on_delete=models.CASCADE)
+    def __str__(self):
+        return self
+
+class Empleado(models.Model):
+    nombre_empleado = models.CharField(max_length=200)
+    correo_empleado = models.CharField(max_length=200)
+    RFC_empleado = models.CharField(max_length=13)
+    id_Empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+    id_Rol = models.ForeignKey(Rol, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.nombre_empleado
--- ocs/empleados/views.py
+++ ocs/empleados/views.py
@@ -4,6 +4,11 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
+from .models import Empleado, Empresa, Rol
 
 class IndexView(generic.ListView):
     template_name = 'empleados/index.html'
+    context_object_name = 'empleados_list'
+
+    def get_queryset(self):
+        return Empresa.objects.all()
