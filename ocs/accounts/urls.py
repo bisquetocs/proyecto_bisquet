563e96c6d4681e0699e34bbc0f5c7d9e637fab23
Fatima <A01209842@itesm.mx>
2018-10-05 04:58:27 -0500

    Combinar empleados

--- ocs/accounts/urls.py
+++ ocs/accounts/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('profile/', views.profile, name='profile'),
+    path('empleados/', views.EmpleadosView.as_view(), name='empleados'),
+    path('empleados/registro/', views.RegisterView.as_view(), name='registro'),
+    path('vote/', views.vote, name='vote'),
     path('', views.home, name='home'),
 ]
--- ocs/accounts/views.py
+++ ocs/accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.urls import *
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
+from django.views import generic
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth import authenticate, login
 
 
@@ -22,6 +24,27 @@ def profile(request):
     current_user = request.user
 
     return render(request, 'profiles/dashboard.html', {
-        'name' : current_user.get_full_name(),
-        'rol' : current_user.groups.all()[0]
+    ##    'name' : current_user.get_full_name(),
+    ##    'rol' : current_user.groups.all()[0]
         })
+
+class EmpleadosView(generic.ListView):
+    template_name = 'empleados/empleados.html'
+    context_object_name = 'empleados_list'
+
+    def get_queryset(self):
+        return User.objects.all()
+
+class RegisterView(generic.ListView):
+    template_name = 'empleados/reg_empleado.html'
+    context_object_name = 'groups_list'
+
+    def get_queryset(self):
+        return Group.objects.all()
+def vote(request):
+    try:
+        empleado = User.objects.get(username=request.POST['inputUser'])
+    except (KeyError, User.DoesNotExist):
+        return HttpResponseRedirect(reverse('registro'))
+    else:
+        return HttpResponseRedirect(reverse('empleados'))
