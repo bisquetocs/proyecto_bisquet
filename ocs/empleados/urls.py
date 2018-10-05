26be22a266d11a371921ced5f7f31f3fe1233e51
Fatima <A01209842@itesm.mx>
2018-10-05 01:00:24 -0500

    resguardo

--- ocs/empleados/urls.py
+++ ocs/empleados/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'empleados'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('registrar/', self.registra_empresa),
+    path('registro/', views.RegisterView.as_view(), name='registro'),
+    path('vote/', views.vote, name='vote'),
+
 
 ]
--- ocs/empleados/views.py
+++ ocs/empleados/views.py
@@ -5,6 +5,10 @@ from django.views import generic
 from django.utils import timezone
 
 from .models import Empleado, Empresa, Rol
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import Group, User
+
+
 
 class IndexView(generic.ListView):
     template_name = 'empleados/empleados.html'
@@ -12,3 +16,18 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Empresa.objects.all()
+
+class RegisterView(generic.ListView):
+    template_name = 'empleados/reg_empleado.html'
+    context_object_name = 'groups_list'
+
+    def get_queryset(self):
+        return Group.objects.all()
+
+def vote(request):
+    try:
+        empleado = User.objects.get(username=request.POST['inputUser'])
+    except (KeyError, User.DoesNotExist):
+        return HttpResponseRedirect(reverse('empleados:registro'))
+    else:
+        return render(request, 'empleados/empleados.html')
