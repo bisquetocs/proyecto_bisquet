d72575b79f12d32fc837fc76dce3c7ed02008f2e
Albertto-1 <A01208219@itesm.mx>
2018-10-06 12:03:20 -0500

    urls de ocs con conflicto arreglado

--- ocs/accounts/views.py
+++ ocs/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+
+def homePage(request):
+    if request.session[]
+    return render(request, 'home/home.html')
