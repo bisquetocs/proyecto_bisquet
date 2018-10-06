ddb45d564c549e90f16ca3d7b47e42a1b724d80b
DanteMaxF <A01207543@itesm.mx>
2018-10-05 19:04:43 -0500

    Terminar Registro de Usuario

--- ocs/accounts/views.py
+++ ocs/accounts/views.py
@@ -3,8 +3,18 @@ from django.contrib.auth.urls import *
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+from django.views import generic
+from django.urls import reverse
+from django.db import models
+
+
+
+
 
 
 
@@ -25,3 +35,31 @@ def profile(request):
         'name' : current_user.get_full_name(),
         'rol' : current_user.groups.all()[0]
         })
+
+def register(request):
+    return render(request, 'registration/register.html')
+
+def registerUser(request):
+    uname = request.POST['username']
+    fName = request.POST['firstName']
+    lName = request.POST['lastName']
+    em = request.POST['email']
+    passwd = request.POST['password']
+    cpasswd = request.POST['confpass']
+    role = request.POST['userRole']
+
+    nu = User(username=uname, first_name=fName, last_name=lName, email=em)
+    nu.set_password(passwd)
+    usertest = User
+    try:
+        usertest = User.objects.get(username=nu.username)
+    except usertest.DoesNotExist:
+        nu.save()
+        g = Group.objects.get(name=role)
+        g.user_set.add(nu)
+        g.save()
+
+        return HttpResponseRedirect('/')
+        # Redireccionar a home
+    else:
+        return HttpResponse('EL usuario ya existe')
