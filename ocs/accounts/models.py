7949b9fd19cec4533d3ced8b51f6d0d012d28bb2
DanteMaxF <A01207543@itesm.mx>
2018-10-08 20:02:46 -0500

    Corregir nombres de clases

--- ocs/accounts/models.py
+++ ocs/accounts/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-from provider.models import proveedor
+from provider.models import Provider
+from franchise.models import Franchise
 
 # Create your models here.
 class OCSUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # id_provider = models.ForeignKey(proveedor, on_delete=models.CASCADE)
-    # id_sucursal = models.ForeignKey(Sucursal, on_delete=models.CASCADE)
+    id_provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
+    id_franchise = models.ForeignKey(Franchise, on_delete=models.CASCADE)
     phone = models.CharField(max_length=10)
     rfc = models.CharField(max_length=12)
     num_ss = models.CharField(max_length=12)
