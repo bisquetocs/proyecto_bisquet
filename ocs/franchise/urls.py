af89ce7f9a445443aae7e6de071bf9c3a96757ea
DanteMaxF <A01207543@itesm.mx>
2018-10-18 18:47:42 -0500

    Mejorar estilo

--- ocs/franchise/urls.py
+++ ocs/franchise/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'franchise'
 urlpatterns = [
     path('register', views.registerFranchise, name='register'),
-    path('home', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('my_providers/', views.my_providers, name='my_providers'),
     path('my_providers/link/', views.link_provider, name='link_provider'),
     path('my_providers/<int:id_provider>', views.provider_detail, name='provider_detail')
