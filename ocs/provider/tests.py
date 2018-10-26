8597fcdc0a66c2d83ad3bdf153cfab5687271fb0
DanteMaxF <A01207543@itesm.mx>
2018-10-25 21:21:26 -0500

    Crear test para LinkCode

--- ocs/provider/tests.py
+++ ocs/provider/tests.py
@@ -1,9 +1,26 @@
 import datetime
-
+from django.contrib.auth.models import User, Group
+from accounts.models import OCSUser
 from django.test import TestCase
 from django.utils import timezone
-
+from django.urls import reverse
 from .models import LinkWithF
+
+def create_user_provider():
+    """
+    Function for creating a user in case it is needed
+    in the test cases
+    """
+    ng = Group(name='Administrador de empresa')
+    ng.save()
+    nu = User(username='uname')
+    nu.set_password('testpasswd123')
+    nu.save()
+    g = Group.objects.get(name='Administrador de empresa')
+    g.user_set.add(nu)
+    g.save()
+    ocsUser = OCSUser(user=nu)
+    ocsUser.save()
 
 
 # Create your tests here.
@@ -21,3 +38,15 @@ class LinkWithFTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=-7)
         passed_link_code = LinkWithF(date_of_creation=time)
         self.assertIs(passed_link_code.check_timeout(), False)
+
+class LinkCodeTest(TestCase):
+
+    def test_user_redirected_after_creating_code(self):
+        """
+            The user is going to be redirected to the same page with a 302 code,
+            after it creates a link Code
+        """
+        user = create_user_provider()
+        self.client.login(username="test", password="testpassword")
+        response = self.client.post('/provider/my_clients/link_code/')
+        self.assertEqual(response.status_code, 302)
