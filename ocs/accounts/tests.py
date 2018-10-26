2dc4d9736a8aef1cc3418cbd8b41d5da7f98c5a5
DanteMaxF <A01207543@itesm.mx>
2018-10-26 00:01:40 -0500

    Terminar tests para logoutFranchise y logoutProvider

--- ocs/accounts/tests.py
+++ ocs/accounts/tests.py
@@ -44,7 +44,7 @@ class UserLoginTest(TestCase):
 
     def test_provider_logged_with_incorrect_user_pass(self):
         """
-            The user wont be able to login to the system in case the user inserts
+            The provider user wont be able to login to the system in case the user inserts
             a wrong username or password
         """
         user = create_user_provider()
@@ -58,7 +58,7 @@ class UserLoginTest(TestCase):
 
     def test_provider_tries_to_enter_home_without_logged_in(self):
         """
-            The user will be redirected to the login form in case the user tries to
+            The provider user will be redirected to the login form in case the user tries to
             access a url that requires a login at the beggining
         """
         user = create_user_provider()
@@ -69,7 +69,7 @@ class UserLoginTest(TestCase):
 
     def test_franchise_logged_with_incorrect_user_pass(self):
         """
-            The user wont be able to login to the system in case the user inserts
+            The franchise user wont be able to login to the system in case the user inserts
             a wrong username or password
         """
         user = create_user_franchise()
@@ -83,7 +83,7 @@ class UserLoginTest(TestCase):
 
     def test_franchise_tries_to_enter_home_without_logged_in(self):
         """
-            The user will be redirected to the login form in case the user tries to
+            The franchise user will be redirected to the login form in case the user tries to
             access a url that requires a login at the beggining
         """
         user = create_user_franchise()
@@ -91,3 +91,28 @@ class UserLoginTest(TestCase):
         response = self.client.get('/franchise/home/')
 
         self.assertRedirects(response, '/accounts/login/?next=/franchise/home/', status_code=302, target_status_code=200, msg_prefix='', fetch_redirect_response=True)
+
+
+class UserLogoutTest(TestCase):
+
+    def test_provider_logged_out(self):
+        """
+            Ther provider user will be redirected to the landing page of the OCS
+            system once the user logout from the system. In order to logout,
+            the user must be logged in
+        """
+        user = create_user_provider()
+        self.client.login(username="uname", password="testpasswd123")
+        response = self.client.get('/accounts/accounts/logout/')
+        self.assertRedirects(response, '/', status_code=302, target_status_code=200, msg_prefix='', fetch_redirect_response=True)
+
+    def test_franchise_logged_out(self):
+        """
+            Ther franchise user will be redirected to the landing page of the OCS
+            system once the user logout from the system. In order to logout,
+            the user must be logged in
+        """
+        user = create_user_franchise()
+        self.client.login(username="uname", password="testpasswd123")
+        response = self.client.get('/accounts/accounts/logout/')
+        self.assertRedirects(response, '/', status_code=302, target_status_code=200, msg_prefix='', fetch_redirect_response=True)
