77b7b53c4d200cbc66843c9c08172d62448939a2
DanteMaxF <A01207543@itesm.mx>
2018-11-10 13:00:55 -0600

    Crear base de datos para reporte de inventarios.

--- ocs/inventory/models.py
+++ ocs/inventory/models.py
@@ -24,3 +24,19 @@ class LinkedInventory(models.Model):
     id_franchise = models.ForeignKey(Franchise, on_delete=models.CASCADE)
     id_product = models.ForeignKey(Product, on_delete=models.CASCADE)
     amount = models.IntegerField()
+
+class PrivateProductRecord(models.Model):
+    id_franchise = models.ForeignKey(Franchise, on_delete=models.CASCADE)
+    id_private_product = models.ForeignKey(PrivateProduct, on_delete=models.CASCADE)
+    date = models.DateField()
+    comment = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    io = models.BooleanField()
+
+class LinkedProductRecord(models.Model):
+    id_franchise = models.ForeignKey(Franchise, on_delete=models.CASCADE)
+    id_linked_product = models.ForeignKey(LinkedInventory, on_delete=models.CASCADE)
+    date = models.DateField()
+    comment = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    io = models.BooleanField()
