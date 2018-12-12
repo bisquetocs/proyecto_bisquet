0830d059e8692837ee206a335e33c401f43f4e11
DanteMaxF <A01207543@itesm.mx>
2018-12-11 18:43:59 -0600

    Collect static files for production

---
+++ ocs/static/js/datatables_records.js
@@ -0,0 +1,29 @@
+$(document).ready( function () {
+
+
+
+    $('table.display').DataTable({
+      scrollY:        '100vh',
+      scrollCollapse: true,
+      "order": [[ 0, "desc" ]],
+      "language": {
+            "lengthMenu": "Mostrar _MENU_ elementos por página",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Página _PAGE_ de _PAGES_",
+            "infoEmpty": "No hay registros disponibles",
+            "infoFiltered": "(filtered from _MAX_ total records)",
+            "search":         "Buscar:",
+            "paginate": {
+                "first":      "Primero",
+                "last":       "Último",
+                "next":       "Siguiente",
+                "previous":   "Anterior"
+            },
+        }
+    });
+
+
+
+
+
+});
